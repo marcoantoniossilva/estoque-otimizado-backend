3f17731c9c9555c3363b32d90e93a87ea6322e6d
marcoantoniossilva <marcoantoniosilva.tec@gmail.com>
2022-10-25 00:57:34 -0300

    ajuste em scripts e configuracao do bd

--- src/main/resources/db/migration/V1__create_boxes_table.sql
+++ src/main/resources/db/migration/V1__create_boxes_table.sql
@@ -1,7 +1,5 @@
-CREATE SEQUENCE boxes_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 100;
-
 CREATE TABLE boxes (
-  id_box smallint check (id > 0) NOT NULL DEFAULT NEXTVAL ('boxes_seq'),
+  id_box varchar(11) NOT NULL,
   street varchar(2) NOT NULL,
   rack varchar(2) NOT NULL,
   box_column varchar(2) NOT NULL,
