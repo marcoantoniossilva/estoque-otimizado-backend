b0c18c05e6620720da7681800512be6ab4d2a530
marcoantoniossilva <marcoantoniosilva.tec@gmail.com>
2022-10-24 21:01:10 -0300

    primeiro commit

---
+++ src/main/java/io/github/marcoantoniossilva/estoqueotimizadobackend/EstoqueOtimizadoApplication.java
@@ -0,0 +1,13 @@
+package io.github.marcoantoniossilva.estoqueotimizadobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueOtimizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueOtimizadoApplication.class, args);
+	}
+
+}
