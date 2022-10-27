1772c7f2bfd9d82b3c897a730769176623b092b6
marcoantoniossilva <marcoantoniosilva.tec@gmail.com>
2022-10-27 20:50:37 -0300

    Criação de migrations

--- src/main/resources/db/migration/V1__create_boxes_table.sql
+++ src/main/resources/db/migration/V1__create_boxes_table.sql
@@ -4,5 +4,11 @@ CREATE TABLE boxes (
   rack varchar(2) NOT NULL,
   box_column varchar(2) NOT NULL,
   line varchar(2) NOT NULL,
-  PRIMARY KEY (id_box)
+  register_in TIMESTAMP(0) DEFAULT NULL,
+  register_by smallint check (register_by > 0) NOT NULL,
+  updated_in TIMESTAMP(0) DEFAULT NULL,
+  updated_by smallint check (updated_by > 0) NOT NULL,
+  PRIMARY KEY (id_box),
+  CONSTRAINT boxes_ibfk_1 FOREIGN KEY (register_by) REFERENCES users (id_user) ON DELETE CASCADE,
+  CONSTRAINT boxes_ibfk_2 FOREIGN KEY (updated_by) REFERENCES users (id_user) ON DELETE CASCADE
 );
--- src/main/resources/db/migration/V2__create_products_table.sql
+++ src/main/resources/db/migration/V2__create_products_table.sql
@@ -5,6 +5,12 @@ CREATE TABLE products (
   description varchar(200) NOT NULL,
   bar_code varchar(13) NOT NULL,
   id_box varchar(11) NOT NULL,
+  register_in TIMESTAMP(0) DEFAULT NULL,
+  register_by smallint check (register_by > 0) NOT NULL,
+  updated_in TIMESTAMP(0) DEFAULT NULL,
+  updated_by smallint check (updated_by > 0) NOT NULL,
   PRIMARY KEY (id_product),
-  CONSTRAINT products_ibfk_1 FOREIGN KEY (id_box) REFERENCES boxes (id_box) ON DELETE CASCADE
+  CONSTRAINT products_ibfk_1 FOREIGN KEY (id_box) REFERENCES boxes (id_box) ON DELETE CASCADE,
+  CONSTRAINT products_ibfk_2 FOREIGN KEY (register_by) REFERENCES users (id_user) ON DELETE CASCADE,
+  CONSTRAINT products_ibfk_3 FOREIGN KEY (updated_by) REFERENCES users (id_user) ON DELETE CASCADE
 );
